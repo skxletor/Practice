af1b42e7d698a64dcdfa0a366e97df6cff05654f
skxletor <jr01@outlook.com>
2026-02-18 22:50:39 -0500

    dont kill me git thank you

--- imdoingsomething.c
+++ imdoingsomething.c
@@ -6,7 +6,12 @@ int main() {
     int age = 67;
     int age2 = 6677;
 
-    printf("i am %d and vro is %d ", age2, age);
+
+    printf("i am %d and vro is %d\n", age2, age);
+
+    age2 = age;
+
+    printf("ur fucvking stupid im %d", age2);
 
 
     return 0;
