096503b43d2e4cd5d8bfa505d91393279a66b417
skxletor <jr01@outlook.com>
2026-02-19 11:10:14 -0500

    how does this work

--- C practice/back2work.c
+++ C practice/back2work.c
@@ -7,11 +7,9 @@ int main(){
     double retardNumber;
     char retardChar;
 
-    printf("gimme a retearded number ");
-    scanf("\n%lf", &retardNumber);
+    printf("gimme a retearded number thne character \n");
+    scanf("%lf %c", &retardNumber, &retardChar);
 
-    printf("gimme a retarded character ");
-    scanf("\n%c", &retardChar);
 
 
     printf("retard = %.0lf%c",retardNumber, retardChar);
