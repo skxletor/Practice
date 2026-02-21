cd803a18d13538a78417d1bd2a265e45748198b6
skxletor <jr01@outlook.com>
2026-02-21 17:44:23 -0500

    wow

--- C practice/bool.c
+++ C practice/bool.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
+
+    bool value1 = (12>6.7);
+
+    int a= printf("%d", value1);
+
+    if (a != 1)
+    {
+        printf("\nwow");
+    }
+    
+
 
 
 
