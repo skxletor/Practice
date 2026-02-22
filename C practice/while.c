e3350fda800960ddac0ccd250172aad861670000
skxletor <jr01@outlook.com>
2026-02-22 18:43:08 -0500

    GAHHHHHHHHHHHHH

--- C practice/while.c
+++ C practice/while.c
@@ -3,14 +3,22 @@
 int main(){
 
 
-    int count =1;
+    int count =10;
 // this shit lowkey a waste of my time get to the good stuff twin
-    while(count<5){
-        count = ++count;
-        printf("%i\n",count);
-        printf("this not gonna go on forever eek\n");
-    }
+    // while(count<5){
+    //     count = ++count;
+    //     printf("%i\n",count);
+    //     printf("this not gonna go on forever eek\n");
+    // }
 
+
+    while (count>0)
+    {
+        int ans = 9*count;
+        printf("9*%d = %d\n",count, ans);
+        count = --count;
+    }
+    
 
 
     return 0;
