b67a2c01e9ef9cb2a8c44371a974dfd039d570d5
skxletor <jr01@outlook.com>
2026-02-22 17:56:47 -0500

    wow i made a calc (shjort for calculator)

--- C practice/blicky.c
+++ C practice/blicky.c
@@ -3,42 +3,105 @@
 int main(){
 
 
-    int number;
+    char oppyDay; //+ - * or /
+    int num1;
+    int num2;
+    int ans;
 
-    printf("number pls guys 1-7 \n");
+    printf("+ - * or /\n");
+    scanf("%c",&oppyDay);
 
-    scanf("%d",&number);
+    printf("what is num 1\n");
+    scanf("%d",&num1);
+
+    printf("what is num 2\n");
+    scanf("%d",&num2);
 
 
-    switch (number)
+    switch (oppyDay)
     {
-    case 1:
-        printf("sdunday");
+    case '+':
+        ans =(num1 + num2);
+        printf("%d", ans);
         break;
 
-    case 2:
-        printf("mundee");
+    case '-':
+        ans =(num1 - num2);
+        printf("%d", ans);
         break;
-    case 3:
-        printf("tusdee");
+    
+    case '*':
+        ans =(num1 * num2);
+        printf("%d", ans);
         break;
-    case 4:
-        printf("wensday");
-        break;
-    case 5:
-        printf("thursdee maybe");
-        break;
-    case 6:
-        printf("friday funday");
-        break;
-    case 7:
-        printf("SIX SEVENN");
+
+    case '/':
+        ans =(num1 / num2);
+        printf("%d", ans);
         break;
     
     default:
-        printf("follow the rules assholle");    
-    break;
+        printf("fuh oyu");
+        break;
     }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int number;
+
+    // printf("number pls guys 1-7 \n");
+
+    // scanf("%d",&number);
+
+
+    // switch (number)
+    // {
+    // case 1:
+    //     printf("sdunday");
+    //     break;
+
+    // case 2:
+    //     printf("mundee");
+    //     break;
+    // case 3:
+    //     printf("tusdee");
+    //     break;
+    // case 4:
+    //     printf("wensday");
+    //     break;
+    // case 5:
+    //     printf("thursdee maybe");
+    //     break;
+    // case 6:
+    //     printf("friday funday");
+    //     break;
+    // case 7:
+    //     printf("SIX SEVENN");
+    //     break;
+    
+    // default:
+    //     printf("follow the rules assholle");    
+    // break;
+    // }
 
 
 
