25d6c9691cbcfb47abc560f08646a42decb323c1
skxletor <jr01@outlook.com>
2026-02-21 21:08:19 -0500

    tsk tsk

--- C practice/ifelse.c
+++ C practice/ifelse.c
@@ -7,8 +7,13 @@ int main(){
     printf("age pls tard \n");
     scanf("%d",&age);
 
-    if (age == (67||76)){
+    
+    if (age == 67||age == 69||age == 76){
         printf("fuck you retard");
+    }
+    else{
+        printf("your age is not retgadred");
+
     }
     
 
