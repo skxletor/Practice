3efe33ea0582f05d34865ec69e0f5a01d0b82e2e
skxletor <jr01@outlook.com>
2026-02-22 18:33:11 -0500

    snore

---
+++ C practice/while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int count =1;
+// this shit lowkey a waste of my time get to the good stuff twin
+    while(count<5){
+        count = ++count;
+        printf("%i\n",count);
+        printf("this not gonna go on forever eek\n");
+    }
+
+
+
+    return 0;
+}
