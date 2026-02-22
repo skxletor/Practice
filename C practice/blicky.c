56b1c52ee13bffb5a51b705737a6e9b781e5d364
skxletor <jr01@outlook.com>
2026-02-22 17:46:34 -0500

    programiz program efficiently: IMPOSSIBLE

--- C practice/blicky.c
+++ C practice/blicky.c
@@ -3,7 +3,44 @@
 int main(){
 
 
+    int number;
+
+    printf("number pls guys 1-7 \n");
+
+    scanf("%d",&number);
+
+
+    switch (number)
+    {
+    case 1:
+        printf("sdunday");
+        break;
+
+    case 2:
+        printf("mundee");
+        break;
+    case 3:
+        printf("tusdee");
+        break;
+    case 4:
+        printf("wensday");
+        break;
+    case 5:
+        printf("thursdee maybe");
+        break;
+    case 6:
+        printf("friday funday");
+        break;
+    case 7:
+        printf("SIX SEVENN");
+        break;
     
+    default:
+        printf("follow the rules assholle");    
+    break;
+    }
+
+
 
     return 0;
 }
