c552e38d59668b23bbfefc565174c0c6a67ba863
skxletor <jr01@outlook.com>
2026-02-27 16:05:21 -0500

    tsk tsk

--- C practice/struct.c
+++ C practice/struct.c
@@ -10,12 +10,16 @@ struct person
 int main(){
 
     struct person person1;
-
+    struct person person2;
     person1.gpa = 1;
     person1.age=4;
 
+    person2.gpa = 4;
+    person2.age=400;
+
     
-    printf("dumb %.1lf and unc %d", person1.gpa,person1.age);
+    printf("dumb %.1lf and unc %d\n", person1.gpa,person1.age);
+    printf("genius %.1lf and wow %d", person2.gpa,person2.age);
 
 
     return 0;
