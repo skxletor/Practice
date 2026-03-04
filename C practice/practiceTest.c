47c00311f2c755cf1b930512d96830e3967d42db
Justin R <jr01@outlook.com>
2026-03-03 22:04:22 -0500

    practice file wish me luck me!!!!

---
+++ C practice/practiceTest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// 1. Start the user with a balance of $1000
+// 2. Show a menu with 3 options: check balance, deposit, and withdraw
+// 3. Let the user pick an option and do the action
+// 4. After each action, show the updated balance and ask if they want to do something else
+// 5. If they try to withdraw more than their balance, print an error message instead of going negative
+// 6. Option to quit the program
+
+int main(){
+
+
+    return 0;
+}
