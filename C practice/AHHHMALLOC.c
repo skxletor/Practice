484dce1424828d55391ba6b70c74033b56a39853
Justin R <jr01@outlook.com>
2026-03-03 20:36:43 -0500

    gahhh

--- C practice/AHHHMALLOC.c
+++ C practice/AHHHMALLOC.c
@@ -3,29 +3,62 @@
 
 int main(){
 
-    int n=5;
-    int* pn;
-    pn =malloc(n * sizeof(int));
+    // int n=5;
+    // int* pn;
+    // pn =malloc(n * sizeof(int));
 
-    if (pn == NULL)
-    {
-        printf("fuh you");
-        return 0;
-    }
+    // if (pn == NULL)
+    // {
+    //     printf("fuh you");
+    //     return 0;
+    // }
 
 
-    for (size_t i = 0; i < n; i++)
-    {
-        scanf("%d",pn+i);
-    }
+    // for (size_t i = 0; i < n; i++)
+    // {
+    //     scanf("%d",pn+i);
+    // }
 
-    for (size_t i = 0; i < n; i++)
-    {
-        printf("\n%d",*(pn+i));
-    }
+    // for (size_t i = 0; i < n; i++)
+    // {
+    //     printf("\n%d",*(pn+i));
+    // }
 
-    free(pn);
+    // free(pn);
     
+    //programming "tacks"
+
+
+    int ages=5;
+
+    int* pAges = malloc(ages * sizeof(int));
+
+
+    for (size_t i = 0; i < ages; i++)
+    {
+        scanf("%d",pAges+i);
+    }
+    
+
+    for (size_t i = 0; i < ages; i++)
+    {
+        printf("\n%d",*(pAges+i));
+    }
+
+    ages=7;
+
+    pAges = realloc(pAges,ages*sizeof(int));
+    
+    pAges[5]=32;
+
+    pAges[6]=59;
+
+    printf("\n");
+    for (size_t i = 0; i < ages; i++)
+    {
+        printf("\n%d",*(pAges+i));
+    }
+
 
     return 0;
 }
