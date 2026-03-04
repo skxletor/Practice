b7b8a5f6f2d0730f49701b3e8f161bf94acefd8a
Justin R <jr01@outlook.com>
2026-03-03 20:08:54 -0500

    this shit is witchcraft

--- C practice/AHHHMALLOC.c
+++ C practice/AHHHMALLOC.c
@@ -2,9 +2,29 @@
 
 int main(){
 
-    int n=100;
+    int n=5;
     int* pn;
     pn =(int*)malloc(n * sizeof(int));
+
+    if (pn == NULL)
+    {
+        printf("fuh you");
+        return 0;
+    }
+
+
+    for (size_t i = 0; i < n; i++)
+    {
+        scanf("%d",pn+i);
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        printf("\n%d",*(pn+i));
+    }
+
+    free(pn);
+    
 
     return 0;
 }
