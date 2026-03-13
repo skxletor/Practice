82dc03c8e4f248864526a549f16de194f0464da9
Justin R <jr01@outlook.com>
2026-03-12 20:17:00 -0400

    wow wow wow

---
+++ Web Practice/jsIntro/main.js
@@ -0,0 +1,12 @@
+console.log("hello vro");
+
+// function add(text){
+    
+//     let newStringU=text[0].toUpperCase()
+//     let newStringL=text.slice(1).toLowerCase()
+//     let newString=newStringU+newStringL
+//     return newString;
+// }
+
+// console.log(add("abcDJAHFhalfhkjHL"));
+
