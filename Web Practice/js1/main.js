29e049bbfe5c110dc5d1f236d8b94daf4b89860c
Justin R <jr01@outlook.com>
2026-03-13 21:02:50 -0400

    ill make sense of this later...

--- Web Practice/js1/main.js
+++ Web Practice/js1/main.js
@@ -5,4 +5,26 @@ const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
+
+const contentH1 = document.createElement("h1");
+contentH1.classList.add("contentH1");
+contentH1.textContent = "im h1!";
+
+
+const contentp = document.createElement("p");
+contentp.classList.add("contentp");
+contentp.textContent = "pee";
+contentp.style.color = "blue";
+
 container.appendChild(content);
+container.appendChild(contentH1);
+container.appendChild(contentp);
+
+const newDiv = document.createElement("div");
+newDiv.classList.add("content");
+newDiv.textContent = "i hate you js!";
+newDiv.style.backgroundColor = "pink";
+newDiv.style.borderWidth = "2px";
+newDiv.style.borderStyle = "solid";
+newDiv.style.borderColor = "black";
+container.appendChild(newDiv);
